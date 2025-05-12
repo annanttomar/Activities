b8f6f9e7add1def631cb795bca57f08927a2e9fc
Anant <anantbizz123@gmail.com>
2025-05-12 20:09:30 +0530

    regExp removed as requested

--- websites/A/AnimeKai/presence.ts
+++ websites/A/AnimeKai/presence.ts
@@ -31,7 +31,7 @@ presence.on(
 
 presence.on('UpdateData', async () => {
   const presenceData: PresenceData = {
-    name: 'AnimeKai',
+    name: 'AnimeKAI',
     largeImageKey: ActivityAssets.Logo,
     startTimestamp: browsingTimestamp,
   }
@@ -39,7 +39,7 @@ presence.on('UpdateData', async () => {
   const buttons = await presence.getSetting<boolean>('buttons')
 
   if (pathname === '/' || pathname === '/home') {
-    presenceData.details = 'Exploring AnimeKai.to'
+    presenceData.details = 'Exploring AnimeKai'
   }
   else if (
     /\/(?:ongoing|recent|movie|new-releases|updates|tv|completed|top-upcoming|ona|ova|special|genres\/.*)/.test(
