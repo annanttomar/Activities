03c1e5a531a0dc607c2911a39b5da2b420c60f70
Anant <anantbizz123@gmail.com>
2025-05-16 18:14:41 +0530

    ASI error Fixes

--- websites/P/Pomofocus/presence.ts
+++ websites/P/Pomofocus/presence.ts
@@ -1,32 +1,32 @@
 const presence = new Presence({
-  clientId: '1350359904300564510',
-});
+  clientId: '1350359904300564510'
+})
 
 const presenceData: PresenceData = {
   details: 'Studying...',
   state: '📖',
-  largeImageKey: 'https://i.ibb.co/TBYtYzMc/940956e1-8b78-424c-ad21-3a5e687dd6b4.jpg',
-};
-
-const emojis = ['📖', '📚', '📕', '📝', '✏️', '🧠', '💡', '📘', '📙', '📗'];
-let emojiIndex = 0;
-
-let stored = sessionStorage.getItem('startTimestamp');
-if (!stored) {
-  stored = Math.floor(Date.now() / 1000).toString();
-  sessionStorage.setItem('startTimestamp', stored);
+  largeImageKey: 'https://i.ibb.co/TBYtYzMc/940956e1-8b78-424c-ad21-3a5e687dd6b4.jpg'
 }
-const startTimestamp = Number.parseInt(stored, 10);
+
+const emojis = ['📖', '📚', '📕', '📝', '✏️', '🧠', '💡', '📘', '📙', '📗']
+let emojiIndex = 0
+
+let stored = sessionStorage.getItem('startTimestamp')
+if (!stored) {
+  stored = Math.floor(Date.now() / 1000).toString()
+  sessionStorage.setItem('startTimestamp', stored)
+}
+const startTimestamp = Number.parseInt(stored, 10)
 
 function updatePresence(): void {
-  presenceData.state = emojis[emojiIndex];
-  presenceData.startTimestamp = startTimestamp;
-  presence.setActivity(presenceData);
-  emojiIndex = (emojiIndex + 1) % emojis.length;
+  presenceData.state = emojis[emojiIndex]
+  presenceData.startTimestamp = startTimestamp
+  presence.setActivity(presenceData)
+  emojiIndex = (emojiIndex + 1) % emojis.length
 }
 
-setInterval(updatePresence, 30000);
+setInterval(updatePresence, 30000)
 
 presence.on('UpdateData', () => {
-  updatePresence();
-});
+  updatePresence()
+})
