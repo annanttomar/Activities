82202d1dd64ba39d63d0da88e59a7f883526fa6f
Anant <anantbizz123@gmail.com>
2025-05-16 22:39:43 +0530

    Changes as requested

---
+++ websites/K/Kuroiru/presence.ts
@@ -0,0 +1,76 @@
+const presence = new Presence({
+  clientId: '1371050079439425576',
+})
+
+let startTimestamp = sessionStorage.getItem('startTimestamp')
+if (!startTimestamp) {
+  startTimestamp = Math.floor(Date.now() / 1000).toString()
+  sessionStorage.setItem('startTimestamp', startTimestamp)
+}
+
+presence.on('UpdateData', async () => {
+  const { pathname, search, hash } = document.location
+
+  const presenceData: PresenceData = {
+    details: '',
+    
+    state: '',
+    largeImageKey: 'https://i.ibb.co/pv5RKDLb/apple-icon-120x120-512x512.png',
+    startTimestamp: Number.parseInt(startTimestamp, 10),
+  }
+
+  let detailsText = 'Browsing Kuroiru'
+  let mediaName = ''
+
+  const isAnime = pathname.startsWith('/anime/')
+  const isManga = pathname.startsWith('/manga/')
+
+  if (isAnime || isManga) {
+    const segments = pathname.split('/')
+    if (segments.length >= 4 && segments[3]) {
+      mediaName = decodeURIComponent(segments[3].replace(/_/g, ' '))
+    }
+
+    if (hash.includes('tab=streams')) {
+      detailsText = 'Checking Streams'
+    }
+    else if (hash.includes('tab=related')) {
+      detailsText = 'Viewing Related Titles'
+    }
+    else if (hash.includes('tab=music')) {
+      detailsText = 'Browsing Music Info'
+    }
+    else if (hash.includes('tab=news')) {
+      detailsText = 'Reading News'
+    }
+    else if (hash.includes('tab=read')) {
+      detailsText = 'Reading Manga'
+    }
+    else {
+      detailsText = isAnime ? 'Reading Anime Info' : 'Reading Manga Info'
+    }
+
+    presenceData.state = mediaName
+  }
+  else if (pathname === '/anime/explore') {
+    detailsText = 'Exploring Anime on Kuroiru'
+  }
+  else if (pathname === '/manga/explore') {
+    detailsText = 'Exploring Manga on Kuroiru'
+  }
+  else if (pathname === '/airing.html') {
+    if (search.includes('filter=upcoming')) {
+      detailsText = 'Browsing Upcoming Anime'
+    }
+    else {
+      detailsText = 'Browsing Airing Anime'
+    }
+  }
+  else if (pathname === '/app') {
+    detailsText = 'Browsing Kuroiru'
+  }
+
+  presenceData.details = detailsText
+
+  presence.setActivity(presenceData)
+})
--- websites/P/Pomofocus/presence.ts
+++ websites/P/Pomofocus/presence.ts
@@ -1,12 +1,6 @@
 const presence = new Presence({
-  clientId: '1350359904300564510',
+  clientId: '1350359904300564510'
 })
-
-const presenceData: PresenceData = {
-  details: 'Idling 💤',
-  state: '',
-  largeImageKey: 'https://i.ibb.co/TBYtYzMc/940956e1-8b78-424c-ad21-3a5e687dd6b4.jpg',
-}
 
 let idleTS = sessionStorage.getItem('idleTimestamp')
 if (!idleTS) {
@@ -16,6 +10,11 @@ if (!idleTS) {
 const idleTimestamp = Number(idleTS)
 
 function updatePresence() {
+  const presenceData: PresenceData = {
+    details: 'Idling 💤',
+    largeImageKey: 'https://i.ibb.co/TBYtYzMc/940956e1-8b78-424c-ad21-3a5e687dd6b4.jpg',
+  }
+
   const title = document.title.trim()
 
   const isFocus = !title.startsWith('25:00 - Time to focus!') && title.includes('Time to focus!')
@@ -25,16 +24,11 @@ function updatePresence() {
   if (isFocus) {
     presenceData.details = 'Focusing 📖'
     presenceData.state = 'for 25 min…'
-    delete presenceData.startTimestamp
-  }
-  else if (isShortBrk || isLongBrk) {
+  } else if (isShortBrk || isLongBrk) {
     presenceData.details = 'Taking a break ☕'
     presenceData.state = 'Chilling ☕'
-    delete presenceData.startTimestamp
-  }
-  else {
+  } else {
     presenceData.details = 'Idling 💤'
-    presenceData.state = ''
     presenceData.startTimestamp = idleTimestamp
   }
 
@@ -42,5 +36,3 @@ function updatePresence() {
 }
 
 presence.on('UpdateData', updatePresence)
-
-setInterval(updatePresence, 1000)
